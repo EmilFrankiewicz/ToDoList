002660caf446b259470398b73fc4f1e33a5b8898
Emil Frankiewicz <emilfrankiewicz2@gmail.com>
2019-09-28 07:56:21 +0200

    added: task, user, tdolist class + repositories+DTO

--- src/main/java/pl/emilfrankiewicz/user/User.java
+++ src/main/java/pl/emilfrankiewicz/user/User.java
@@ -1,35 +1,39 @@
 package pl.emilfrankiewicz.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import pl.emilfrankiewicz.list.ToDoList;
 
 @Entity
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public Long id;
+	private Long id;
 
-	public String username;
+	private String username;
 
-	public String password;
+	private String password;
+
+	@OneToMany(mappedBy = "toDoListOwner", cascade = CascadeType.ALL)
+	private List<ToDoList> toDoList;
 
 	public User() {
+		setToDoList(new ArrayList<>());
 	}
 
 	public User(String username, String password) {
+		this();
 		this.username = username;
 		this.password = password;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public String getUsername() {
@@ -46,6 +50,14 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<ToDoList> getToDoList() {
+		return toDoList;
+	}
+
+	public void setToDoList(List<ToDoList> toDoList) {
+		this.toDoList = toDoList;
 	}
 
 }
